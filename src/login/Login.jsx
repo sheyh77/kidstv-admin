0e9f55c0d4e7448665b7bafe98db885756149b43
sheyh77 <ubabaniyozov@gmail.com>
2025-08-21 18:21:39 +0500

    auth add

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,56 +1,57 @@
-import React from 'react';
-import { Button, Form, message } from 'antd';
+import React, { useContext } from 'react';
+import { Button, Form, Input, message } from 'antd';
 import axios from 'axios';
+import { AuthContext } from '../context/AuthContext';
 
 function Login() {
 
   const [messageApi, contextHolder] = message.useMessage()
-
+  const { setUser } = useContext(AuthContext)
   const onFinish = values => {
-    console.log(values)
-  }
+    axios.post("https://ceed8a646c7fba8b.mokky.dev/auth", values).
+      then(res => {
+        if (res.status === 201) {
+          localStorage.setItem("token", res.data.token)
+          setUser(res)
+        }
+      }).
+      catch(err => {
+        if (err.status = 401) {
+          messageApi.open({
+            type: "error",
+            content: "Login xato"
+          })
+        }
+      })
 
-  const onFinishFailed = values => {
-    console.log(values)
   }
-
-  // const onFinish = values => {
-  //   axios.post("https://ceed8a646c7fba8b.mokky.dev/auth", values).
-  //     then(res => {
-  //       if (res.status === 201) {
-  //         localStorage.setItem("token", res.data.token)
-  //         console.log("togri")
-  //       }
-  //     }).
-  //     catch(err => {
-  //       if (err.status = 401) {
-  //         // messageApi.open({
-  //         //   type: "error",
-  //         //   content: "Login xato"
-  //         // })
-  //         console.log("error")
-  //       }
-  //     })
-  // }
 
   return (
-    <section className="login">
-      <div className="login-box">
-        <h1 className="login-box-title">Log In</h1>
-        <Form
-          name='basic'
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete='off'
-          className='login-form'
-        >
-          <input type="text" className='login-box-inp' placeholder='Username' />
-          <input type="password" className='login-box-inp' placeholder='Password' />
-          <Button className="login-box-btn" onClick={onFinish} type='primary' htmlType='submit'>Login</Button>
-        </Form>
-      </div>
-    </section>
+    <>
+      {contextHolder}
+      <section className="login">
+        <div className="login-box">
+          <h1 className="login-box-title">Log In</h1>
+          <Form
+            name='basic'
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            autoComplete='off'
+            className='login-form'
+          >
+            <Form.Item name="username" className='login-boxes'>
+              <Input placeholder='Username' />
+            </Form.Item>
+            <Form.Item name="password" className='login-boxes'>
+              <Input type="password" className='login-box-inp' placeholder='Password' />
+            </Form.Item>
+            <Form.Item className='login-boxes'>
+              <Button onClick={onFinish} type='primary' htmlType='submit'>Login</Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </section >
+    </>
   )
 }
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
-import { dashboardCardMap } from '../data/dashboardCardMap'
-import Item from 'antd/es/list/Item'
+import React, { useContext } from 'react';
+import { dashboardCardMap } from '../data/dashboardCardMap';
+import { Button, Popconfirm } from 'antd';
+import { AuthContext } from '../context/AuthContext';
 
 function Dashboard() {
+
+  const { Logout } = useContext(AuthContext)
+
+  const confirm = () => {
+    Logout()
+  };
+
   return (
     <section className="dashboard">
-      <h1 className="dashboard-title">Dashboard admin</h1>
+      <div className="dashboard-about">
+        <h1 className="dashboard-title">Dashboard admin</h1>
+        <Popconfirm
+          placement='bottomRight'
+          title="Log out"
+          description="Rostdan ham chiqmoqchimisiz?"
+          onConfirm={confirm}
+          okText="Yes"
+          cancelText="No"
+        >
+          <Button danger>Log out</Button>
+        </Popconfirm>
+      </div>
       <div className="dashboard-card">
         {
           dashboardCardMap.map((Item) => (
